b5381de196bb5b17b134526c27bfbde022e02a8f
Jamil khan <jamilkhan@Jamils-MacBook-Pro.local>
2022-10-29 10:21:10 +0530

    message

--- config/db.js
+++ config/db.js
@@ -1,6 +1,6 @@
 import mongoose from "mongoose"
 const hostconnect = async () => {
-    const conn = await mongoose.connect(" mongodb+srv://prod:UbpiFvFrOIIRpowd@pixel.rjqqvvg.mongodb.net/?retryWrites=true&w=majority/deshwali")
+    const conn = await mongoose.connect("mongodb+srv://vakeel:vakeel4100@pixel.rjqqvvg.mongodb.net/?retryWrites=true&w=majority")
     console.log("host-----", conn.connection.host)
 }
 export default hostconnect;
--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 import express from "express"
 import router from "./routes/text.route.js";
 import hostconnect from "./config/db.js";
+import { config } from "dotenv";
 const app = express();
 app.use(express.json());
 app.use(router)
 hostconnect();
+config();
 app.listen(process.env.PORT || 3000,(req,res)=>{
     console.log("server is run on 2000 port");
     
