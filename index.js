92327b6ecf2a88dd20ba8f93aaa1c309a8f48f9b
Jamil khan <jamilkhan@Jamils-MacBook-Pro.local>
2022-10-31 11:22:16 +0530

    add env

--- index.js
+++ index.js
@@ -3,10 +3,10 @@ import router from "./routes/text.route.js";
 import hostconnect from "./config/db.js";
 import { config } from "dotenv";
 const app = express();
+config();
 app.use(express.json());
 app.use(router)
 hostconnect();
-config();
 app.listen(process.env.PORT || 3000,(req,res)=>{
     console.log("server is run on 2000 port");    
 })
